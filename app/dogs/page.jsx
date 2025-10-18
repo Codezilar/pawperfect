42cbc0aaf4129a6c303093a09fa9637dcd6a1186
codezilar <zoeehimen@gmail.com>
2025-10-17 22:19:36 -0700

    location remove

--- app/dogs/page.jsx
+++ app/dogs/page.jsx
@@ -57,6 +57,7 @@ const page = () => {
                                     <p className='sweet'>{dog.desc  }</p>
                                     <div className='gold'>
                                         <span><LuCalendar /> <p>{dog.breed}</p><LuDot /><p>{dog.age}</p></span>
+                                        <span><IoLocationOutline /> <p>{dog.location}</p></span>
                                         <span className='dolas'>
                                             <LuDollarSign /> 
                                             <h1>${dog.price}</h1>
